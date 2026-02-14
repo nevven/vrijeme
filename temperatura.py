af69da6d1e7d8fef64c36ed857965c0fb085a86d
nevenb <regbackup@gmail.com>
2026-02-14 11:53:40 +0100

    feat: change temperatura.py to use icons from shared.py

--- temperatura.py
+++ temperatura.py
@@ -1,7 +1,7 @@
 import requests
 import time
 from rich import print
-from shared import BASE_URL, LATITUDE, LONGITUDE, TIMEZONE, weather_codes, wind_direction_to_compass
+from shared import BASE_URL, LATITUDE, LONGITUDE, TIMEZONE, weather_icons, wind_direction_to_compass
 
 params = {
     'latitude': LATITUDE,
@@ -14,7 +14,7 @@ response = requests.get(BASE_URL, params=params)
 data = response.json()
 
 temperature = round(data['current']['temperature_2m'])
-current_weather = weather_codes[data['current']['weather_code']]
+weather_icon = weather_icons[data['current']['weather_code']]
 humidity = data['current']['relative_humidity_2m']
 wind_speed = data['current']['wind_speed_10m']
 wind_gust = data['current']['wind_gusts_10m']
@@ -22,5 +22,5 @@ wind_direction = wind_direction_to_compass(data['current']['wind_direction_10m']
 
 current_time = time.strftime('%H:%M')
 
-print(f'Zagreb [{current_time}] [cyan]{temperature}c[/cyan] | [cyan]{current_weather}[/cyan] | '
+print(f'Zagreb [{current_time}] {weather_icon} [cyan]{temperature}c[/cyan] | '
       f'Vlaga [cyan]{humidity}%[/cyan] | Vjetar [cyan]{wind_speed}/{wind_gust}[/cyan] km/h [cyan]{wind_direction}[/cyan]\n')
